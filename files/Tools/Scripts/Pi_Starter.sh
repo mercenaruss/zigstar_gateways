7b19ff6330271af5d313a14befd66c12cb38d4fa
mercenaruss <mercenaruss@gmail.com>
2023-11-18 19:18:02 +0100

    Update Pi_Starter.sh

--- files/Tools/Scripts/Pi_Starter.sh
+++ files/Tools/Scripts/Pi_Starter.sh
@@ -74,7 +74,7 @@ echo
 #Choose to add to init
 read -p "$(echo -e ${RED}"Do you want to enable RTC on boot?(y/n)?"${NC})" choice
 case "$choice" in 
-  y|Y ) echo -e "${GREEN}Yes, RTC is enabled on boot${NC}"; sudo sed -i 's/exit 0//' /etc/rc.local; sudo sh -c "echo 'sudo echo pcf8563 0x51 > /sys/class/i2c-adapter/i2c-1/new_device' >> /etc/rc.local"; sudo sh -c "echo 'sudo hwclock -s' >> /etc/rc.local"; sudo sh -c "echo 'exit 0' >> /etc/rc.local";;
+  y|Y ) echo -e "${GREEN}Yes, RTC is enabled on boot${NC}"; sudo sed -i 's/exit 0//' /etc/rc.local; sudo sh -c "echo 'sudo echo pcf8563 0x51 > /sys/class/i2c-adapter/i2c-1/new_device' >> /etc/rc.local"; sudo sh -c "echo 'sudo hwclock -s' >> /etc/rc.local"; sudo sh -c "echo 'exit 0' >> /etc/rc.local"; sudo sh -c "echo 'echo RTC Clock is enabled' >> /home/pi/.bashrc"; sudo sh -c "echo 'sudo hwclock -r' >> /home/pi/.bashrc";;
   n|N ) echo -e "${RED}No, you will active manually${NC}";;
   * ) echo "invalid";;
 esac
